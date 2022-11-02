9c2562be495516aaaf1237b500906f904a22a0f1
Najma <najma.a16@gmail.com>
2022-11-02 17:27:44 -0500

    commit tree e04b7aaf238c8240b9200355c001ddcafbd3fdd2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, birthday) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] + "," + " for the wonderful " + birthday + " gift!")
+    }
+    return messages;
+  }
+
+
+
+function countDown(){
+ let count = 10
+while (count >= 0){
+    count--;
+    console.log(count);
+}
+}
